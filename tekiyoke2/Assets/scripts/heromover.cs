6ca86c8ab8ba64aae23432a00033e90d9453145b
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-03-03 04:04:08 +0900

    hero's sprite changes

--- tekiyoke2/Assets/scripts/heromover.cs
+++ tekiyoke2/Assets/scripts/heromover.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class heromover : MonoBehaviour
 {
-    private static float moveSpeed = 0.2f;
-    private static float jumpSpeed = 0.8f;
-    private float speedY = 0f;
-    private static float gravity = 0.05f;
-    private static float groundHeight = -1.5f;
+    public static float moveSpeed = 0.2f;
+    public static float jumpSpeed = 0.8f;
+    public float speedY = 0f;
+    public static float gravity = 0.05f;
+    public static float groundHeight = -1.5f;
+    public SpriteRenderer spriteRenderer;
+    public Sprite jumpingSprite;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,9 @@ public class heromover : MonoBehaviour
         // 接地判定
         if(this.transform.position.y <= groundHeight){
             this.transform.position = new Vector3(this.transform.position.x,groundHeight);
-            if(speedY < 0){speedY = 0;}
+            if(speedY < 0){
+                speedY = 0;
+            }
         }
 
         this.transform.position += new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed, 0);
@@ -31,6 +35,7 @@ public class heromover : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.UpArrow) && this.transform.position.y <= groundHeight){
             speedY = jumpSpeed;
+            spriteRenderer.sprite = jumpingSprite; // これでええのん？
         }
     }
 }
