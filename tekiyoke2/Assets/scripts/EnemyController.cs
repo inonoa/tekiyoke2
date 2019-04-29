73e88523f0e2a106551e80ff7f0a11ede3823aa5
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-04-30 03:31:05 +0900

    敵を作った、ダメージはまだない

---
+++ tekiyoke2/Assets/scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.MovePosition(new Vector3(transform.position.x-1,transform.position.y));
+    }
+}
+//null
