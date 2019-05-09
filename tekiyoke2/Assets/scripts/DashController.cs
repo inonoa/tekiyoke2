259348377a197b55257e27b9a5c0d9a2f7e078f6
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-05-09 12:58:04 +0900

    comment

--- tekiyoke2/Assets/scripts/DashController.cs
+++ tekiyoke2/Assets/scripts/DashController.cs
@@ -6,26 +6,39 @@ using System;
 public class DashController : MonoBehaviour
 {
 
+    ///<summary>ダッシュの状態を表すenum</summary>
     public enum DState{
         Off, StandingBy, Dashing, InCoolTime,
     }
+
+    ///<summary>ダッシュの状態を表す</summary>
     public DState state = DState.Off;
+
+    ///<summary>タメ終了(ダッシュ開始)からダッシュ終了にかかる時間。タメ終了時に毎回セットされる</summary>
     private int dashFullTime;
+
+    ///<summary>タメ終了から何F経過したか？</summary>
     public int dashTime = 0;
 
+
+    ///<summary>フレームごとの移動距離</summary>
     float dashX = 0;
 
+
+    ///<summary>ダッシュボタンを押したときにタメが開始されるか？</summary>
     private bool CanDash{
         get{
             return state==DState.Off;
         }
     }
 
-
+    ///<summary>フレームごとの移動距離の配列。タメ終了時にタメの強さに応じていい感じの値が格納される</summary>
     private float[] moveDists;
     // x = (4999 - 4900*(t/T)^2) * ((t/T)^2 /99) * X (Tはダッシュに要する時間、Xは距離)の曲線に従って移動する。
     //ここで、 T[F] = X[Unit] * 3/20 くらいである。
 
+    ///<summary>タメ終了時に呼ぶ。ための強さに応じてmoveDistsに移動距離を格納し、ダッシュ中に遷移。</summary>
+    ///<param name="velocity">タメの強さ(って何)</param>
     void ExecuteDash(float velocity){
         int x = 200;
         int t = (x*3) /20;
@@ -49,6 +62,7 @@ public class DashController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // ダッシュ中なら次の移動距離を準備。
         if(state==DState.Dashing){
             dashX = moveDists[dashTime];
             dashTime ++;
@@ -56,6 +70,7 @@ public class DashController : MonoBehaviour
                 state = DState.InCoolTime;
             }
         }
+        // ダッシュ後なら残りcool timeを減らす
         else if(state==DState.InCoolTime){
             dashTime --;
             if(dashTime==0){
