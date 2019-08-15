8c711895d90b98e2ed8b815b6190cad491e8e6e9
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-08-16 08:55:08 +0900

    Xで停止(StateがWindに)

--- tekiyoke2/Assets/scripts/TitleScene/CloudSpawner.cs
+++ tekiyoke2/Assets/scripts/TitleScene/CloudSpawner.cs
@@ -5,6 +5,12 @@ using System;
 
 public class CloudSpawner : MonoBehaviour
 {
+    public enum State{
+        Active, Wind, Inactive
+    }
+
+    public State state = State.Active;
+
     public List<GameObject> clouds2Spawn = new List<GameObject>();
     private List<GameObject> cloudsExisting = new List<GameObject>();
 
@@ -23,21 +29,23 @@ public class CloudSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //追加
-        count ++;
-        if(count==count2Spawn){
-            count = 0;
-            int idx2Spawn = random.Next(clouds2Spawn.Count);
-            Vector3 position2Spawn = new Vector3(random.Next(800,1000),random.Next(-500,500),random.Next(-3,0));
-            cloudsExisting.Add(Instantiate(clouds2Spawn[idx2Spawn],position2Spawn,Quaternion.identity));
-        }
+        if(state==State.Active){
+            //追加
+            count ++;
+            if(count==count2Spawn){
+                count = 0;
+                int idx2Spawn = random.Next(clouds2Spawn.Count);
+                Vector3 position2Spawn = new Vector3(random.Next(800,1000),random.Next(-500,500),random.Next(-3,0));
+                cloudsExisting.Add(Instantiate(clouds2Spawn[idx2Spawn],position2Spawn,Quaternion.identity));
+            }
 
-        //移動、削除
-        for(int i=cloudsExisting.Count-1;i>-1;i--){
-            cloudsExisting[i].transform.position += new Vector3(-moveSpeed,0,0);
-            if(cloudsExisting[i].transform.position.x < -800){
-                Destroy(cloudsExisting[i]);
-                cloudsExisting.RemoveAt(i);
+            //移動、削除
+            for(int i=cloudsExisting.Count-1;i>-1;i--){
+                cloudsExisting[i].transform.position += new Vector3(-moveSpeed,0,0);
+                if(cloudsExisting[i].transform.position.x < -800){
+                    Destroy(cloudsExisting[i]);
+                    cloudsExisting.RemoveAt(i);
+                }
             }
         }
     }
