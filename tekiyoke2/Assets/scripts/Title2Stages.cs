db210bb7f54cffa58af8953ebed5940216e0915b
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-08-17 23:46:05 +0900

    初期状態の雲増量

--- tekiyoke2/Assets/scripts/Title2Stages.cs
+++ tekiyoke2/Assets/scripts/Title2Stages.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Title2Stages : MonoBehaviour
 {
     public CloudSpawner clouds;
+    public GameObject pressaAnyKey;
+
+    bool buttonPushed = false;
+
+    int count = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +20,19 @@ public class Title2Stages : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!buttonPushed){
+            count ++;
+            if(count==40){
+                pressaAnyKey.SetActive(false);
+            }else if(count==80){
+                pressaAnyKey.SetActive(true);
+                count = 0;
+            }
+        }
+
         if(Input.GetKeyDown(KeyCode.Z)){
             clouds.state = CloudSpawner.State.Wind;
+            buttonPushed = true;
         }
     }
 }
--- tekiyoke2/Assets/scripts/TitleScene/CloudSpawner.cs
+++ tekiyoke2/Assets/scripts/TitleScene/CloudSpawner.cs
@@ -29,7 +29,11 @@ public class CloudSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i=0;i<3;i++){
+            int idx2Spawn = random.Next(clouds2Spawn.Count);
+            Vector3 position2Spawn = new Vector3(random.Next(-600,600),random.Next(-400,400),random.Next(-3,0));
+            cloudsExisting.Add(Instantiate(clouds2Spawn[idx2Spawn],position2Spawn,Quaternion.identity));
+        }
     }
 
     // Update is called once per frame
