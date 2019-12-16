fb570ac0055371c9d5da9ec9fcd485ca1230fba6
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-12-17 03:19:03 +0900

    Gierが浮遊してくる

--- tekiyoke2/Assets/scripts/Enemies/GierController.cs
+++ tekiyoke2/Assets/scripts/Enemies/GierController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class GierController : EnemyController
 {
+    //あとでクラス化するか…？
+    enum GierState{ BeforeFinding, FindingNow, Running }
+    GierState state = GierState.BeforeFinding;
+    int findingCount = 0;
+
+    [SerializeField]
+    float runSpeed = 5;
+
+
     // Start is called before the first frame update
     new void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(state){
+
+            case GierState.BeforeFinding:
+                if( MyMath.DistanceXY(HeroDefiner.CurrentHeroPos,transform.position) > 500 ){
+                    state = GierState.FindingNow;
+                }
+                break;
+
+            case GierState.FindingNow:
+                findingCount ++;
+                if(findingCount > 80){
+                    state = GierState.Running;
+                    findingCount = 0;
+                }
+                break;
+
+            case GierState.Running:
+                if(HeroDefiner.CurrentHeroPos.x > transform.position.x) MovePos(runSpeed,0);
+                else MovePos(-runSpeed,0);
+                break;
+        }
     }
 }
---
+++ tekiyoke2/Assets/scripts/MyMath.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MyMath
+{
+    public static float DistanceXY(Vector3 vec1, Vector3 vec2){
+        return (float) Math.Sqrt( (vec1.x - vec2.x) * (vec1.x - vec2.x) + (vec1.y - vec2.y) * (vec1.y - vec2.y) );
+    }
+}
