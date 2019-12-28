dc5c46e182069e3323855d0659ac74c479830aa0
inonoa <himura.kennsinn.degozaru@gmail.com>
2019-12-29 05:12:22 +0900

    DはDPのD、PはDPのPです(DPCDとDPの概念を作った)

---
+++ tekiyoke2/Assets/scripts/DPCD.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DPCD : MonoBehaviour
+{
+    static readonly int rotateInterval = 10;
+    int rotateCount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateCount ++;
+        rotateCount %= rotateInterval;
+        if(rotateCount==0) transform.Rotate(new Vector3(0,0,45));
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag == "Player"){
+            Destroy(gameObject);
+            DPManager.Instance.AddDP(1);
+        }
+    }
+}
---
+++ tekiyoke2/Assets/scripts/Hero/DPManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DPManager : MonoBehaviour
+{
+    public static DPManager Instance{ get; set; }
+
+    int DP = 0;
+    static readonly int maxDP = 100;
+
+    public void AddDP(int delta){
+        if(delta > 0){
+            DP = Math.Min(maxDP, DP + delta);
+            print(delta + "DP、ゲットだぜ！  |  今のDPは" + DP + "Pだぜ！！");
+        }
+        else print("負のDPは得られません");
+    }
+
+    public bool UseDP(int dp2Use){
+        if(DP >= dp2Use){
+            DP -= dp2Use;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
