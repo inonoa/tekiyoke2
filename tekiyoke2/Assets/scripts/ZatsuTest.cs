aa61945f11471c76c0f5b93f8a98b2dd4053057e
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-01-09 15:49:50 +0900

    testする側も消した

--- tekiyoke2/Assets/scripts/ZatsuTest.cs
+++ tekiyoke2/Assets/scripts/ZatsuTest.cs
@@ -13,6 +13,6 @@ public class ZatsuTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(InputManager.Instance.GetButton(ButtonCode.Zone)) RingBuffer<int>.Test();
+        
     }
 }
