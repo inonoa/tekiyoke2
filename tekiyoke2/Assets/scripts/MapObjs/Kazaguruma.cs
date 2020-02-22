934457eea5cb271643e0056653eb10a5a948f334
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-02-22 17:29:31 +0900

    コードは書いた

---
+++ tekiyoke2/Assets/scripts/MapObjs/Kazaguruma.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Kazaguruma : MonoBehaviour{
+    [SerializeField] Transform kuruma;
+    [SerializeField] float rotateVelFirst = 0.1f;
+    [SerializeField] float nextRotateVelRate = 0.9f;
+    float rotateVel;
+    bool rotating = false;
+    public bool IsRotating{
+        get => rotating;
+    }
+    public event EventHandler Rotated;
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag=="Jet"){
+            rotateVel = rotateVelFirst;
+            Rotated?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rotateVel > 0){
+            kuruma.Rotate(0,0,rotateVel);
+            rotateVel *= nextRotateVelRate;
+            if(rotateVel < 0.001f) rotateVel = 0;
+        }
+    }
+}
---
+++ tekiyoke2/Assets/scripts/MapObjs/KazagurumaObserver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class KazagurumaObserver : MonoBehaviour
+{
+    [SerializeField] Kazaguruma[] kazagurumas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(kazagurumas.All(kg => kg.IsRotating)){
+            Debug.Log("回った！！");
+        }
+    }
+}
