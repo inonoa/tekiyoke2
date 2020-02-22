0cf2c21c2a150cd6cce74678db9497022302134b
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-02-22 15:47:25 +0900

    EnemySpawner(主人公が近づくとスポーンする)

---
+++ tekiyoke2/Assets/scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemySpawner : MonoBehaviour
+{
+    EnemyController[] enemies;
+
+    Vector3[] defaultPositions;
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.tag=="CanSeeEnemy"){
+            foreach(EnemyController enemy in enemies) enemy.gameObject.SetActive(true);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = GetComponentsInChildren<EnemyController>(true);
+        defaultPositions = enemies.Select(e => e.transform.position).ToArray();
+    }
+}
