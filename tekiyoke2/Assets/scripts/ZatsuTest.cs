f53adc62afd9877d0f95c3e3609f516629bb35eb
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-02-24 06:42:22 +0900

    DOTweenに屈した、CanvasとCameraとかの設定かえた

--- tekiyoke2/Assets/scripts/ZatsuTest.cs
+++ tekiyoke2/Assets/scripts/ZatsuTest.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class ZatsuTest : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Image>().DOFade(1,1f);
+        transform.DOLocalMoveX(400,1f).SetEase(Ease.OutQuint);
     }
 
     // Update is called once per frame
