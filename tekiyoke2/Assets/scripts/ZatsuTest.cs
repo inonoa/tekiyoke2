31cca0fc4aa69e9e8b6519f6651212b543998966
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-02-24 08:08:57 +0900

    シェーダーをアレした

--- tekiyoke2/Assets/scripts/ZatsuTest.cs
+++ tekiyoke2/Assets/scripts/ZatsuTest.cs
@@ -9,8 +9,8 @@ public class ZatsuTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Image>().DOFade(1,1f);
-        transform.DOLocalMoveX(400,1f).SetEase(Ease.OutQuint);
+        GetComponent<Image>().DOFade(0.7f,1f);
+        transform.DOLocalMoveX(0,1f).SetEase(Ease.OutQuint);
     }
 
     // Update is called once per frame
