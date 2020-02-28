bbebbb612c4ce31d3b3ebe31f2e70f8b0141ecf7
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-02-28 18:01:03 +0900

    セーブ時になんか∂

--- tekiyoke2/Assets/scripts/Hero/SavePositionManager.cs
+++ tekiyoke2/Assets/scripts/Hero/SavePositionManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class SavePositionManager : MonoBehaviour
 {
@@ -8,6 +10,7 @@ public class SavePositionManager : MonoBehaviour
 
     [SerializeField]
     int saveCostDP = 10;
+    [SerializeField] Image dialogImg;
 
     public void Try2Save(){
         if(DPManager.Instance.DP >= saveCostDP){
@@ -16,6 +19,20 @@ public class SavePositionManager : MonoBehaviour
             MemoryOverDeath.Instance.Save();
             resPos.SetActive(true);
             resPos.transform.position = new Vector3(transform.position.x, transform.position.y, resPos.transform.position.z);
+
+            //応急
+            Sequence dialogSeq = DOTween.Sequence();
+
+            dialogSeq.Append(dialogImg.DOFade(1, 0.5f));
+            dialogSeq.Join  (dialogImg.transform.DOLocalMoveX(0, 0.5f).SetEase(Ease.OutSine));
+
+            dialogSeq.Append(dialogImg.transform.DOLocalMoveX(0, 1f));
+
+            dialogSeq.Append(dialogImg.DOFade(0, 0.5f));
+            dialogSeq.Join  (dialogImg.transform.DOLocalMoveX(-100, 0.5f).SetEase(Ease.InSine));
+
+            dialogSeq.Append(dialogImg.transform.DOLocalMoveX(100, 0f));
+
         }else{
             print("セーブに失敗しました。");
         }
