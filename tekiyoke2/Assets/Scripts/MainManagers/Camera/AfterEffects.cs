d89a057dda1732164cb824d8fa618959d133a466
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-03-06 03:45:45 +0900

    matを複数に

---
+++ tekiyoke2/Assets/Scripts/MainManagers/Camera/AfterEffects.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterEffects : MonoBehaviour
+{
+    [SerializeField] Material[] mats;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst){
+        foreach(Material mt in mats){
+            Graphics.Blit(src, dst, mt);
+        }
+    }
+}
