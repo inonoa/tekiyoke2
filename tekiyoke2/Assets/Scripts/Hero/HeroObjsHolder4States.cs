f285fae275019d9552e375d6286ddd8f86446919
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-04-09 04:21:23 +0900

    壁沿いでなんか出る

--- tekiyoke2/Assets/Scripts/Hero/HeroObjsHolder4States.cs
+++ tekiyoke2/Assets/Scripts/Hero/HeroObjsHolder4States.cs
@@ -8,17 +8,20 @@ public class HeroObjsHolder4States : MonoBehaviour
     public GameObject jetstreamPrefab;
     public TrailRenderer jetTrail;
 
-    static readonly int tsuchihokoriMaxNum = 10;
+    //このへんなんとかならんの……
     public ObjectPool<Tsuchihokori> TsuchihokoriPool{ get; private set; }
     [SerializeField] Tsuchihokori tsuchihokoriForRun;
     public ObjectPool<JumpEffect> JumpEffectPool{ get; private set; }
     [SerializeField] JumpEffect jumpEffectPrefab;
     public ObjectPool<JumpEffect> JumpEffectInAirPool{ get; private set; }
     [SerializeField] JumpEffect jumpEffectInAirPrefab;
+    public ObjectPool<Kabezuri> kabezuriPool{ get; private set; }
+    [SerializeField] Kabezuri kabezuriPrefab;
 
     void Start(){
         TsuchihokoriPool = new ObjectPool<Tsuchihokori>(tsuchihokoriForRun, 8, transform.parent);
         JumpEffectPool = new ObjectPool<JumpEffect>(jumpEffectPrefab, 8, transform.parent);
         JumpEffectInAirPool = new ObjectPool<JumpEffect>(jumpEffectInAirPrefab, 8, transform.parent);
+        kabezuriPool = new ObjectPool<Kabezuri>(kabezuriPrefab, 8, transform.parent);
     }
 }
---
+++ tekiyoke2/Assets/Scripts/Hero/Kabezuri.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Kabezuri : MonoBehaviour, IReusable
+{
+    [SerializeField] Vector3 positionFromHero;
+    SimpleAnim anim;
+    SpriteRenderer spriteRenderer; 
+
+    Tween[] tweensToKill = new Tween[3];
+    
+    void Awake()
+    {
+        anim = GetComponent<SimpleAnim>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Activate(string wallDirStr){
+        InUse = true;
+
+        foreach(Tween tw in tweensToKill) tw?.Kill();
+
+        transform.position = HeroDefiner.CurrentHeroPos + new Vector3(
+            wallDirStr=="r" ? positionFromHero.x : -positionFromHero.x,
+            positionFromHero.y,
+            positionFromHero.z
+        );
+        transform.localScale = new Vector3(wallDirStr=="r" ? 1 : -1, 1, 1);
+        spriteRenderer.color = new Color(1,1,1,1);
+
+        anim.ResetAndStartAnim(() => {
+            tweensToKill[0] = transform.DOMoveY(100,0.5f).SetRelative();
+            tweensToKill[1] = spriteRenderer.DOFade(0, 0.5f);
+            tweensToKill[2] = DOVirtual.DelayedCall(0.5f, () => {
+                gameObject.SetActive(false);
+                InUse = false;
+            });
+        });
+    }
+
+    public bool InUse{ get; private set; }
+}
