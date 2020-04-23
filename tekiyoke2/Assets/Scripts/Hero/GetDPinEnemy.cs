0f85d1a1c8b5f8c8406c6aad0d56ea6d2e5c707a
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-04-23 20:10:37 +0900

    霧の色だけ替えた（（ここでやるなよ…）

---
+++ tekiyoke2/Assets/Scripts/Hero/GetDPinEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using DG.Tweening;
+
+public class GetDPinEnemy : MonoBehaviour
+{
+    public event EventHandler gotDP;
+
+    ///<summary>敵のソウル的なのからDPを奪う、光ってからフェードアウトする</summary>
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag=="Enemy"){
+            print("dpppp");
+            var dPinEnemy = other.GetComponentInParent<EnemyController>().DPCD;
+            dPinEnemy.Light();
+            DOVirtual.DelayedCall(1f,() => dPinEnemy.FadeOut());
+            gotDP?.Invoke(dPinEnemy.CollectDP(), EventArgs.Empty);
+        }
+    }
+}
