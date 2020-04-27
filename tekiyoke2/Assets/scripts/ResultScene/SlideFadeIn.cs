ed1e0cca10e1d243014a1cd5f674da329ef17b66
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-04-28 04:00:57 +0900

    リザルト画面が新しくなった（）

--- tekiyoke2/Assets/scripts/ResultScene/SlideFadeIn.cs
+++ tekiyoke2/Assets/scripts/ResultScene/SlideFadeIn.cs
@@ -5,10 +5,13 @@ public class SlideFadeIn : MonoBehaviour
 {
     [SerializeField] float secToIn = 1;
     [SerializeField] float secDelay = 0;
-    [SerializeField] Vector3 distanceVec = new Vector3(1000,0,0);
+    [SerializeField] Vector3 distanceVec = new Vector3(100,0,0);
     
     void Start()
     {
+        transform.position -= distanceVec;
+        CanvasGroup canvasGrp = GetComponent<CanvasGroup>();
+        canvasGrp.alpha = 0;
         DOVirtual.DelayedCall(secDelay, () => {
             transform.DOLocalMove(distanceVec, secToIn).SetEase(Ease.OutQuint).SetRelative();
             GetComponent<CanvasGroup>().DOFade(1, secToIn);
