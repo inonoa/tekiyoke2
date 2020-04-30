00821b266e661988c9970f0e57ab80e444953a53
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-05-01 08:13:24 +0900

    DPCD取得時もほわんってなる

--- tekiyoke2/Assets/scripts/MapObjs/DPCD.cs
+++ tekiyoke2/Assets/scripts/MapObjs/DPCD.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DPCD : MonoBehaviour
 {
@@ -9,17 +10,36 @@ public class DPCD : MonoBehaviour
     [SerializeField] float rotateSpeed = 20;
     [SerializeField] int DPperDPCD = 1;
 
+    bool gotten = false;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] SpriteRenderer lightRenderer;
+
     void Update()
     {
         rotateCount ++;
         rotateCount %= rotateInterval;
-        if(rotateCount==0) transform.Rotate(new Vector3(0,0,rotateSpeed));
+        if(rotateCount==0) transform.Rotate(new Vector3(0,0, gotten? rotateSpeed*2 : rotateSpeed));
     }
 
     void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.tag == "Player"){
-            Destroy(gameObject);
+        if(other.gameObject.tag == "Player" && !gotten){
+            gotten = true;
             DPManager.Instance.AddDP(DPperDPCD);
+
+            GottenAnimation();
         }
+    }
+
+    void GottenAnimation(){
+        Sequence gottenSeq = DOTween.Sequence();
+
+        float fadeSec = 0.1f;
+        gottenSeq.Append(lightRenderer.DOFade(1,fadeSec));
+        float lightSec = 0.1f;
+        gottenSeq.AppendInterval(lightSec);
+        float dieSec = 0.2f;
+        gottenSeq.Append(lightRenderer.DOFade(0, dieSec));
+        gottenSeq.Join(spriteRenderer.DOFade(0, dieSec));
+        gottenSeq.OnComplete(() => Destroy(gameObject));
     }
 }
