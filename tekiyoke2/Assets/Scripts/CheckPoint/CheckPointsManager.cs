fb0bbc0ab2d9b4cfe5563269e57607cab1d9d1e8
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-10-23 04:46:47 +0900

    チェックポイントとManagerのおおまかなあれ

---
+++ tekiyoke2/Assets/Scripts/CheckPoint/CheckPointsManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Sirenix.OdinInspector;
+
+public class CheckPointsManager : MonoBehaviour
+{
+    [SerializeField] CheckPoint[] checkPoints;
+
+    [SerializeField] [ReadOnly] int frontLine = -1;
+    
+    void Start()
+    {
+        for(int i = 0; i < checkPoints.Length; i++)
+        {
+            int index = i;
+            checkPoints[index].Passed.Subscribe(cp =>
+            {
+                if(frontLine < index)
+                {
+                    frontLine = index;
+                    // "チェックポイント通過"
+                    print($"チェックポイント: {cp.Name}");
+                }
+                else if(frontLine > index)
+                {
+                    // "既に奥のチェックポイントを通過しています"
+                    // これ必要かなあ
+                    print($"チェックポイント: {cp.Name} より奥のチェックポイントを通過済み");
+                }
+                else
+                {
+                    Debug.LogError($"チェックポイント: {cp.Name} に二回目の通過！！");
+                }
+            });
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
