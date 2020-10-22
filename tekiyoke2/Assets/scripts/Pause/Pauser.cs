a2e379b70d6e334672199cfef0c2493c4171eae3
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-10-22 15:39:08 +0900

    ちょっと描き直し

--- tekiyoke2/Assets/scripts/Pause/Pauser.cs
+++ tekiyoke2/Assets/scripts/Pause/Pauser.cs
@@ -10,14 +10,11 @@ public class Pauser : MonoBehaviour
 {
 
     ///<summary>ポーズ画面に貼り付けるスクショ</summary>
-    Image scshoImg;
+    [SerializeField] Image scshoImg;
 
     //ポーズ/ゲーム切り替える親玉
-    GameObject pauseMaster;
-    GameObject gameMaster;
-    
-    ///<summary>スクショ(ポーズに入るときに撮って背景に映す)</summary>
-    Texture2D scsho;
+    [SerializeField] GameObject pauseMaster;
+    [SerializeField] GameObject gameMaster;
 
     ///<summary>いまポーズ中？</summary>
     bool inPause = false;
@@ -42,10 +39,7 @@ public class Pauser : MonoBehaviour
 
     void Start()
     {
-        gameMaster = DraftManager.CurrentInstance.GameMasterTF.gameObject;
-        pauseMaster = DraftManager.CurrentInstance.PauseMasterTF.gameObject;
-        uiMover = pauseMaster.GetComponent<PauseUIMover>();
-        scshoImg = pauseMaster.transform.Find("Canvas").Find("ScSho").GetComponent<Image>();
+        uiMover    = pauseMaster.GetComponent<PauseUIMover>();
         soundGroup = GetComponent<SoundGroup>();
 
         uiMover.pauseEnd += PauseEnded;
@@ -67,7 +61,12 @@ public class Pauser : MonoBehaviour
         //(実際にはフレーム終了後に移行)
         CameraController.CurrentCamera.ScSho(ss =>
         {
-            scshoImg.sprite = Sprite.Create(ss, new Rect(0, 0, Screen.width, Screen.height),new Vector2(0.5f,0.5f));
+            scshoImg.sprite = Sprite.Create
+            (
+                ss,
+                new Rect(0, 0, Screen.width, Screen.height),
+                new Vector2(0.5f, 0.5f)
+            );
 
             gameMaster.SetActive(false);
             pauseMaster.SetActive(true);
