b9ef41266824541a6022cf52dff5dc2ed0914b2b
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-11-02 19:24:28 +0900

    ray実装にした

--- tekiyoke2/Assets/Scripts/Hero/Actions/SakamichiSensors.cs
+++ tekiyoke2/Assets/Scripts/Hero/Actions/SakamichiSensors.cs
@@ -2,30 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using System.Linq;
+using System;
 
 public class SakamichiSensors : MonoBehaviour
 {
-    [SerializeField] ContactFilter2D filter;
-
     [field: SerializeField, ReadOnly, LabelText("L")]
     public bool L{ get; private set; } = false;
 
     [field: SerializeField, ReadOnly, LabelText("R")]
     public bool R{ get; private set; } = false;
 
-    [SerializeField, ReadOnly] bool touchedL = true;
-    [SerializeField, ReadOnly] bool touchedR = true;
+    [SerializeField] SakamichiSensor sensorL;
+    [SerializeField] SakamichiSensor sensorR;
 
-    [SerializeField] Collider2D triggerL;
-    [SerializeField] Collider2D triggerR;
+    [SerializeField] HeroMover hero;
 
     
     void Update()
     {
-        touchedL = triggerL.IsTouching(filter);
-        touchedR = triggerR.IsTouching(filter);
+        sensorL.Update_(hero);
+        sensorR.Update_(hero);
 
-        L = touchedL && !touchedR;
-        R = touchedR && !touchedL;
+        L = sensorL.IsTouching && !sensorR.IsTouching;
+        R = sensorR.IsTouching && !sensorL.IsTouching;
+    }
+}
+
+[Serializable]
+public class SakamichiSensor
+{
+    [field: SerializeField, ReadOnly, LabelText("Touching")]
+    public bool IsTouching{ get; private set; } = false;
+
+    [SerializeField] ContactFilter2D filter;
+
+    RaycastHit2D[] hits = new RaycastHit2D[128];
+    public void Update_(HeroMover hero)
+    {
+        Vector2 origin = hero.transform.position + new Vector3(0, -53);
+        int num_hits = Physics2D.Raycast(origin, new Vector2(0, -1), filter, hits, 45);
+        IsTouching = num_hits > 0;
     }
 }
