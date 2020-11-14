2e8746ff0a8cac1f026971d1f14359cc61ead710
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-11-14 13:59:43 +0900

    共通化

--- tekiyoke2/Assets/Scripts/Hero/HPController.cs
+++ tekiyoke2/Assets/Scripts/Hero/HPController.cs
@@ -28,15 +28,10 @@ public class HPController : MonoBehaviour
     [Space(10)]
     [FoldoutGroup(DEP), SerializeField] Image image;
 
-    private static int max_hp = 3; //正直3で決め打ってるし必要ある？
-    private bool isDamaging = false;
-    private float secondsAfterDamage = 0;
-
-    private float secondsAfterRecover = 0;
     [SerializeField] float mutekiSecondsAfterDamage = 1.6f;
 
 
-    [SerializeField, ReadOnly, InlineProperty] string[] mutekiFilters = new string[0];
+    [SerializeField, ReadOnly] string[] mutekiFilters = new string[0];
     public bool CanBeDamaged => mutekiFilters.Length == 0;
     public void AddMutekiFilter(string key)
     {
@@ -50,11 +45,7 @@ public class HPController : MonoBehaviour
     
 
     ///<summary>ここを直接書き換えない</summary>
-    int hp = max_hp;
-
-    public event EventHandler die;
-    public event EventHandler damaged;
-    public event EventHandler hpChanged;
+    int hp = 3;
 
     new CameraController camera;
     [SerializeField] Vector3 cameraShakeWidth   = new Vector3(30, 30, 0);
@@ -64,7 +55,6 @@ public class HPController : MonoBehaviour
     [SerializeField] float houtaiBlueSeconds = 0.7f;
     [SerializeField] float houtaiAlpha1Seconds = 1f;
 
-    [SerializeField] HeroMover hero;
 
     ///<summary>HPの増減はすべてここから。</summary>
     public void ChangeHP(int value)
@@ -72,87 +62,56 @@ public class HPController : MonoBehaviour
         if(value <= 0 && HP <= 0) return;
         if(value >= 3 && HP >= 3) return;
 
-        if(HP > value)
+        if(value < HP)
         {
-            OnDamaged();
-            damaged?.Invoke(this, EventArgs.Empty);
-
-            if(value <= 0)
-            {
-                die?.Invoke(this, EventArgs.Empty);
-                hp=0;
-
-                image.sprite = img1_0;
-                DOVirtual.DelayedCall(houtaiRedSeconds, () => image.sprite = img0)
-                    .GetPausable().AddTo(this);
-                DOVirtual.DelayedCall(houtaiAlpha1Seconds, () => image.color = Color.white)
-                    .GetPausable().AddTo(this);
-            }
-            else
-            {
-                hp = Math.Min(3, value);
-
-                if(value == 1)
-                {
-                    image.sprite = img2_1;
-                    DOVirtual.DelayedCall(houtaiRedSeconds, () => image.sprite = img1)
-                        .GetPausable().AddTo(this);
-                    DOVirtual.DelayedCall(houtaiAlpha1Seconds, () => image.color = Color.white)
-                        .GetPausable().AddTo(this);
-                }
-                else if(value == 2)
-                {
-                    image.sprite = img3_2;
-                    DOVirtual.DelayedCall(houtaiRedSeconds, () => image.sprite = img2)
-                        .GetPausable().AddTo(this);
-                    DOVirtual.DelayedCall(houtaiAlpha1Seconds, () => image.color = Color.white)
-                        .GetPausable().AddTo(this);
-                }
-            }
-            hpChanged?.Invoke(this, EventArgs.Empty);
+            if(value <= 0)      OnDamaged(img1_0, img0);
+            else if(value == 1) OnDamaged(img2_1, img1);
+            else if(value == 2) OnDamaged(img3_2, img2);
         }
-        else if(HP < value)
+        else if(value > HP)
         {
-            if(value >= 3)
-            {
-                hp = 3;
-                image.sprite = img2_3;
-                DOVirtual.DelayedCall(houtaiBlueSeconds, () => image.sprite = img3)
-                    .GetPausable().AddTo(this);
-                DOVirtual.DelayedCall(houtaiAlpha1Seconds, () => image.color = Color.white)
-                    .GetPausable().AddTo(this);
-                secondsAfterRecover = 0;
-                image.color = new Color(1,1,1,1);
-            }
-            else if(value == 2)
-            {
-                hp = 2;
-                image.sprite = img1_2;
-                DOVirtual.DelayedCall(houtaiBlueSeconds, () => image.sprite = img3)
-                    .GetPausable().AddTo(this);
-                DOVirtual.DelayedCall(houtaiAlpha1Seconds, () => image.color = Color.white)
-                    .GetPausable().AddTo(this);
-                secondsAfterRecover = 0;
-                image.color = new Color(1,1,1,1);
-            }
-            hpChanged?.Invoke(this, EventArgs.Empty);
+            if     (value == 3) OnHealed(img2_3, img3);
+            else if(value == 2) OnHealed(img1_2, img2);
         }
 
-        void OnDamaged()
-        {
-            secondsAfterDamage = 0;
-            isDamaging = true;
-            image.color = new Color(1,1,1,1);
-            AddMutekiFilter("Damage");
-
-            camera.transform.DOShakePosition(cameraShakeSeconds, cameraShakeWidth, cameraShakeVibrato);
-        }
+        hp = value;
     }
 
-    public int HP{ get => hp; }
+    void OnDamaged(Sprite spriteBeingDamaged, Sprite spriteAfterDamage)
+    {
+        const string DMG = "Damage";
+        AddMutekiFilter(DMG);
+        DOVirtual.DelayedCall(mutekiSecondsAfterDamage, () => RemoveMutekiFilter(DMG))
+            .GetPausable()
+            .AddTo(this);
 
-    ///<summary>全回復</summary>
-    public void FullRecover() => ChangeHP(max_hp);
+        camera.transform.DOShakePosition(cameraShakeSeconds, cameraShakeWidth, cameraShakeVibrato);
+
+        image.color  = Color.white;
+        image.sprite = spriteBeingDamaged;
+
+        DOVirtual.DelayedCall(houtaiRedSeconds,    () => image.sprite = spriteAfterDamage)
+            .GetPausable()
+            .AddTo(this);
+        DOVirtual.DelayedCall(houtaiAlpha1Seconds, () => image.color  = new Color(1, 1, 1, 0.7f))
+            .GetPausable()
+            .AddTo(this);
+    }
+
+    void OnHealed(Sprite spriteBeingHealed, Sprite spriteAfterHeal)
+    {
+        image.color  = Color.white;
+        image.sprite = spriteBeingHealed;
+
+        DOVirtual.DelayedCall(houtaiBlueSeconds,   () => image.sprite = spriteAfterHeal)
+            .GetPausable()
+            .AddTo(this);
+        DOVirtual.DelayedCall(houtaiAlpha1Seconds, () => image.color  = new Color(1, 1, 1, 0.7f))
+            .GetPausable()
+            .AddTo(this);
+    }
+
+    public int HP => hp;
 
     void Start()
     {
@@ -162,56 +121,5 @@ public class HPController : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.H)) ChangeHP(3);
-
-        if(this.isDamaging)
-        {
-            // if(secondsAfterDamage < damagemove.GetLength(0))
-            // {
-            //     camera.transform.localPosition += new Vector3(damagemove[(int)(secondsAfterDamage * 50), 0], damagemove[(int)(secondsAfterDamage * 50), 1]);
-            // }
-            // else if(secondsAfterDamage == damagemove.GetLength(0))
-            // {
-            //     camera.transform.localPosition = HeroDefiner.CurrentHeroPastPos[0] + new Vector3(0,50,-200); 
-            //     //短時間に複数回被弾したときに画面揺れの途中で揺れの状態が初めに戻って二重にずれてる、応急処置
-            // }
-
-            // if(secondsAfterDamage < secondsAfterRecover)
-            // {
-            //     if(secondsAfterDamage == 19 || secondsAfterDamage == 20) //これはなぜ
-            //     {
-            //         if(HP==0)      image.sprite = img0;
-            //         else if(HP==1) image.sprite = img1;
-            //         else if(HP==2) image.sprite = img2;
-            //     }
-            //     else if(secondsAfterDamage == 60)
-            //     {
-            //         image.color = new Color(1, 1, 1, 180f/255f);
-            //         isDamaging = false;
-            //     }
-            // }
-        }
-
-        float dt = TimeManager.Current.DeltaTimeAroundHero;
-
-        if(secondsAfterDamage >= mutekiSecondsAfterDamage) return;
-
-        secondsAfterDamage += dt;
-
-        if(secondsAfterDamage >= mutekiSecondsAfterDamage) RemoveMutekiFilter("Damage");
-
-        // if(secondsAfterRecover < secondsAfterDamage)
-        // {
-        //     if (secondsAfterRecover == 40)
-        //     {
-        //         if(HP==2)      image.sprite = img2;
-        //         else if(HP==3) image.sprite = img3;
-        //     }
-        //     else if(secondsAfterRecover == 60)
-        //     {
-        //         image.color = new Color(1,1,1,180f/255f);
-        //     }
-        // }
-
-        secondsAfterRecover += dt;
     }
 }
