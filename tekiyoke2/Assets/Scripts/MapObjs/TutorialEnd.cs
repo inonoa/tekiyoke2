4e1305db39256dbd41c5b1d15eff9ad64bf95cb7
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-11-27 00:18:51 +0900

    チュートリアルからDraft1に直行

---
+++ tekiyoke2/Assets/Scripts/MapObjs/TutorialEnd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEnd : MonoBehaviour
+{
+    bool goaled = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag(TagNames.Hero) && !goaled)
+        {
+            goaled = true;
+            HeroDefiner.currentHero.OnGoal();
+            CameraController.CurrentCamera.Freeze(10);
+            GameTimeCounter.CurrentInstance.DoesTick = false;
+            MemoryOverDeath.Instance.Clear();
+
+            SceneTransition.Start2ChangeScene("Draft1", SceneTransition.TransitionType.Normal);
+        }
+    }
+}
