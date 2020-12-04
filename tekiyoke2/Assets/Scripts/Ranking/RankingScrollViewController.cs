89ef3418200d6297aa82c62fc9dfb5527919dba8
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-12-04 23:37:35 +0900

    キーボードからスクロールできる

---
+++ tekiyoke2/Assets/Scripts/Ranking/RankingScrollViewController.cs
@@ -0,0 +1,54 @@
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingScrollViewController : MonoBehaviour
+{
+    [SerializeField] ScrollRect scrollRect;
+    
+    [SerializeField] float scrollSpeedMax = 200f;
+    [SerializeField] float scrollForce = 100f;
+    [SerializeField] float resistanceRate = 5;
+
+    [SerializeField] bool focused;
+
+    void Update()
+    {
+        if(!focused) return;
+
+        float dt = Time.deltaTime;
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            scrollRect.velocity += Vector2.down * scrollForce * dt;
+            if (scrollRect.velocity.y < -scrollSpeedMax)
+            {
+                scrollRect.velocity = new Vector2(0, -scrollSpeedMax);
+            }
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            scrollRect.velocity += Vector2.up * scrollForce * dt;
+            if (scrollRect.velocity.y > scrollSpeedMax)
+            {
+                scrollRect.velocity = new Vector2(0, scrollSpeedMax);
+            }
+        }
+        else if(scrollRect.velocity.y > 0)
+        {
+            scrollRect.velocity += Vector2.down * scrollForce * dt * resistanceRate;
+            if (scrollRect.velocity.y < 0)
+            {
+                scrollRect.velocity = Vector2.zero;
+            }
+        }
+        else if (scrollRect.velocity.y < 0)
+        {
+            scrollRect.velocity += Vector2.up * scrollForce * dt * resistanceRate;
+            if (scrollRect.velocity.y > 0)
+            {
+                scrollRect.velocity = Vector2.zero;
+            }
+        }
+    }
+}
