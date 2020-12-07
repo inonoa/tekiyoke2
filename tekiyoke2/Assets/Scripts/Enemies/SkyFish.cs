a2311b70d00823d2fb0073a6a5ee91451b76b53e
inonoa <himura.kennsinn.degozaru@gmail.com>
2020-12-07 14:42:38 +0900

    有の敵を作った

--- tekiyoke2/Assets/Scripts/Enemies/SkyFish.cs
+++ tekiyoke2/Assets/Scripts/Enemies/SkyFish.cs
@@ -1,8 +1,30 @@
+using System;
+using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class SkyFish : MonoBehaviour, IHaveDPinEnemy
 {
+    [SerializeField] bool eyeToRight = false;
+    
+    [field: Space(10)]
     [field: SerializeField, LabelText(nameof(DPCD))]
     public DPinEnemy DPCD { get; private set; }
+
+    [SerializeField] DOTweenPath DOTweenPath;
+    
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite spriteLeft;
+    [SerializeField] Sprite spriteRight;
+
+    void Start()
+    {
+        spriteRenderer.sprite = eyeToRight ? spriteRight : spriteLeft;
+        DOTweenPath.tween.GetPausable();
+        DOTweenPath.tween.onStepComplete += () =>
+        {
+            eyeToRight = !eyeToRight;
+            spriteRenderer.sprite = eyeToRight ? spriteRight : spriteLeft;
+        };
+    }
 }
