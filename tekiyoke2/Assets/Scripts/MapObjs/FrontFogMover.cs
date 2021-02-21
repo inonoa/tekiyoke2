13b8215c6796418cdca310a12f492b9f5a77c494
inonoa <himura.kennsinn.degozaru@gmail.com>
2021-02-21 22:45:33 +0900

    フェードイン・アウト

--- tekiyoke2/Assets/Scripts/MapObjs/FrontFogMover.cs
+++ tekiyoke2/Assets/Scripts/MapObjs/FrontFogMover.cs
@@ -1,4 +1,6 @@
 using System;
+using DG.Tweening;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,15 +8,22 @@ public class FrontFogMover : MonoBehaviour
 {
     [SerializeField] float selfSpeed = 0.1f;
     [SerializeField] float speedRateFromHero = 0.1f;
+    [SerializeField] float fadeInDuration = 3f;
+    [SerializeField] float fadeOutDuration = 3f;
+    [SerializeField] float fadeInOutGradationWidth = 2;
     
     [SerializeField] Image image;
     Material material;
     
     static readonly int OffsetX = Shader.PropertyToID("_OffsetX");
+    static readonly int Alpha0UVX = Shader.PropertyToID("_Alpha0uvX");
+    static readonly int Alpha1UVX = Shader.PropertyToID("_Alpha1uvX");
 
     void Awake()
     {
         material = image.material;
+        material.SetFloat(Alpha0UVX, 1);
+        material.SetFloat(Alpha1UVX, 1 + fadeInOutGradationWidth);
     }
 
     void Update()
@@ -22,5 +31,69 @@ public class FrontFogMover : MonoBehaviour
         float currentOffset = material.GetFloat(OffsetX);
         float delta = (selfSpeed + HeroDefiner.currentHero.velocity.X * speedRateFromHero) * TimeManager.Current.DeltaTimeExceptHero;
         material.SetFloat(OffsetX, currentOffset + delta);
+    }
+    
+    Tween currentFade;
+
+    [Button]
+    public void FadeIn(LR direction)
+    {
+        currentFade?.Kill();
+
+        switch (direction)
+        {
+            case LR.L:
+                if (material.GetFloat(Alpha0UVX) > material.GetFloat(Alpha1UVX))
+                {
+                    material.SetFloat(Alpha0UVX, 1);
+                    material.SetFloat(Alpha1UVX, 1 + fadeInOutGradationWidth);
+                }
+                currentFade = DOTween.Sequence()
+                    .Append(material.To(Alpha0UVX, -fadeInOutGradationWidth, fadeInDuration))
+                    .Join(material.To(Alpha1UVX, 0, fadeInDuration));
+                break;
+            
+            case LR.R:
+                if (material.GetFloat(Alpha0UVX) < material.GetFloat(Alpha1UVX))
+                {
+                    material.SetFloat(Alpha0UVX, 0);
+                    material.SetFloat(Alpha1UVX, - fadeInOutGradationWidth);
+                }
+                currentFade = DOTween.Sequence()
+                    .Append(material.To(Alpha0UVX, 1 + fadeInOutGradationWidth, fadeInDuration))
+                    .Join(material.To(Alpha1UVX, 1, fadeInDuration));
+                break;
+        }
+    }
+
+    [Button]
+    public void FadeOut(LR direction)
+    {
+        currentFade?.Kill();
+        
+        switch (direction)
+        {
+            case LR.L:
+                if (material.GetFloat(Alpha0UVX) < material.GetFloat(Alpha1UVX))
+                {
+                    material.SetFloat(Alpha0UVX, 1 + fadeInOutGradationWidth);
+                    material.SetFloat(Alpha1UVX, 1);
+                }
+                currentFade = DOTween.Sequence()
+                    .Append(material.To(Alpha0UVX, 0, fadeOutDuration))
+                    .Join(material.To(Alpha1UVX, - fadeInOutGradationWidth, fadeOutDuration));
+                break;
+            
+            case LR.R:
+                if (material.GetFloat(Alpha0UVX) > material.GetFloat(Alpha1UVX))
+                {
+                    material.SetFloat(Alpha0UVX, - fadeInOutGradationWidth);
+                    material.SetFloat(Alpha1UVX, 0);
+                }
+                currentFade = DOTween.Sequence()
+                    .Append(material.To(Alpha0UVX, 1, fadeOutDuration))
+                    .Join(material.To(Alpha1UVX, 1 + fadeInOutGradationWidth, fadeOutDuration));
+                break;
+        }
     }
 }
---
+++ tekiyoke2/Assets/Scripts/Utils/LR.cs
@@ -0,0 +1,5 @@
+
+public enum LR
+{
+    L, R
+}
